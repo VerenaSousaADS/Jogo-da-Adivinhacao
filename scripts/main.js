4652a8c73cf4bae129061509cca7716ec6ef177c
Verena Sousa <verena.ads19@hotmail.com>
2022-12-25 16:11:29 -0300

    Refatorando

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,28 @@
+//Variáveis
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
 //PEsquisa através do seletor css
-
 const screen1 = document.querySelector('.screen1')
 const screen2 =  document.querySelector('.screen2')
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
+//Eventos
+
+
+//Adiciona o evento que escuta o click e chama a função handleTryClick
+
+btnTry.addEventListener('click', handleTryClick)
+//Adiciona o evento que escuta o click e executa a função anonima que remove a classe hide da classe screen1 e adiciona a classe screen2
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+})
+
+
+//FUNÇÕES
 //função callback
 function handleTryClick(event){
     event.preventDefault() // não faça o padrão
@@ -14,11 +32,8 @@ function handleTryClick(event){
     // Se a variável inputNumber for igual a variável randomNumber
 
     if(Number(inputNumber.value) == randomNumber){
-        //então se adiciona a classe screen1  a classe hide
-        screen1.classList.add('hide')        
 
-        // e se remove a classe hide da classe screen2
-        screen2.classList.remove('hide')
+        toggleScreen()
 
         // no h2 se insere o texto a seguir (fazendo a pesquisa dentro do elemento)
         screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
@@ -31,18 +46,15 @@ function handleTryClick(event){
     console.log(randomNumber)
 }
 
-//Eventos
 
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-//Adiciona o evento que escuta o click e chama a função handleTryClick
-
-btnTry.addEventListener('click', handleTryClick)
-//Adiciona o evento que escuta o click e executa a função anonima que remove a classe hide da classe screen1 e adiciona a classe screen2
-btnReset.addEventListener('click', function () {
-    screen1.classList.remove('hide')
-    screen2.classList.add('hide')
-    //Reinicia a contagem da variavel xAttempts
+function handleResetClick() {
+    toggleScreen()
     xAttempts = 1
-});
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen(){
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+
+};
